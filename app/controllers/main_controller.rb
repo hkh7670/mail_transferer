3c900848542fda0e4116a1b5ec9fbe3dc453376a
Gyuho <hkh7670@gmail.com>
2016-04-09 18:52:27 +0000

    mail practice

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,12 @@
+class MainController < ApplicationController
+    def index
+        
+        
+    end
+    
+    def write
+        @title = params[:title]
+        @content = params[:content]
+    end
+    
+end
