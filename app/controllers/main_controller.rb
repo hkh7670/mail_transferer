dc529d994294b92610eeca200de17d60cf26e31b
Gyuho <hkh7670@gmail.com>
2016-04-27 16:40:08 +0000

    added db function!

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,4 @@
+require 'mailgun'
 class MainController < ApplicationController
     def index
         
@@ -5,8 +6,33 @@ class MainController < ApplicationController
     end
     
     def write
+        
+        @email = params[:email]
         @title = params[:title]
         @content = params[:content]
+        
+        mg_client = Mailgun::Client.new("key-ec3ca6dd228d53d56e9d1b3415469222")
+        message_params =  {
+                   from: 'hkh7670@gmail.com',
+                   to:   @email,
+                   subject: @title,
+                   text:    @content
+                  }
+                  
+        result = mg_client.send_message('sandbox0b8b8ef931aa4621ba89bc43c52ec318.mailgun.org', message_params).to_h!
+        message_id = result['id']
+        message = result['message']
+        
+        new_post = Post.new
+        new_post.title = params[:title]
+        new_post.email = params[:email]
+        new_post.content = params[:content]
+        new_post.save
+        
+        redirect_to "/list"
     end
     
+    def list
+        @every_post = Post.all.order("id desc")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   root 'main#index'
   get '/' => 'main#index'
-  get '/write' => 'main#write'
+  get 'write' => "main#write"
+  get 'list' => "main#list"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
